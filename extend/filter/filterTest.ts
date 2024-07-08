41aa79f44223a2e0d2835f5c73d48ad235704e81
yujiajia <yujiajia@asteria.com>
2024-07-08 17:24:45 +0800

    /post

---
+++ extend/filter/filterTest.ts
@@ -0,0 +1,4 @@
+export const name = 'filterTestUser';
+export const command = (data: string) => { 
+    return data + 1
+}
---
+++ extend/function/functionTest.ts
@@ -0,0 +1,4 @@
+export const name = 'functionTestUser';
+export const command = () => { 
+    return 'functionTestUser'
+}
